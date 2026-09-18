ad20e2ead397204fc7ec8ed0f8a9b5cbb703679f
IrtezaHaider01 <haiderirteza372@gmail.com>
2026-09-18 21:17:51 +0500

    Create app.js

---
+++ JS-04 Ass/app.js
@@ -0,0 +1,97 @@
+// // q1:
+// var colors = ["Red", "Blue", "Green", "Yellow"];
+// console.log(colors[0])
+// console.log(colors[3])
+// colors[1] = "Black"
+// console.log(colors)
+
+// // q2:
+// var student = {
+//     name: "Ali",
+//     age: 20,
+//     city: "Karachi"
+// };
+// console.log(student.name)
+// console.log(student.city)
+// student.city = "Lahore"
+// console.log(student)
+
+// // q3:
+// var students = [
+//     {
+//         name: "Ali",
+//         marks: 70,
+//         atten: true
+//     },
+//     {
+//         name: "Ahmed",
+//         marks: 50,
+//         atten: false
+//     },
+//     {
+//         name: "Zubair",
+//         marks: 90,
+//         atten: true
+//     }
+// ];
+// console.log(students[0].marks)
+// console.log(students[1].atten)
+// students[2].marks = 95
+// students[1].atten = true
+// console.log(students)
+
+// // q4:
+// var students = [
+//     {
+//         name: "Ali",
+//         marks: 70,
+//         atten: true
+//     },
+//     {
+//         name: "Ahmed",
+//         marks: 50,
+//         atten: false
+//     },
+//     {
+//         name: "Zubair",
+//         marks: 90,
+//         atten: true
+//     }
+// ]
+// for (var i = 0; i < students.length; i++) {
+//     console.log(students[i].name + " - " + students[i].marks + " - " + students[i].atten)
+// }
+
+// // q5:
+// var students = [
+//     {
+//         name: "Ali",
+//         marks: 70,
+//         atten: true
+//     },
+//     {
+//         name: "Ahmed",
+//         marks: 50,
+//         atten: false
+//     },
+//     {
+//         name: "Zubair",
+//         marks: 90,
+//         atten: true
+//     }
+// ]
+
+// for (var i = 0; i < students.length; i++) {
+//     students[i].marks = students[i].marks + 5
+// }
+
+// console.log(students)
+
+// var totalMarks = 0;
+
+// for (var i = 0; i < students.length; i++) {
+//     totalMarks = totalMarks + students[i].marks
+// }
+
+// console.log(totalMarks)
+// console.log(totalMarks / students.length)
