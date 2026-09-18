87f708becc1f7bafd1f770930e96422696ff1c51
IrtezaHaider01 <haiderirteza372@gmail.com>
2026-09-18 21:26:06 +0500

    Add files via upload

---
+++ JS-08/app.js
@@ -0,0 +1,23 @@
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let task = input.value;
+
+    if (task === "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    let li = document.createElement("li");
+
+    li.innerHTML = task + ' <button class="delete" onclick="deleteTask(this)">Delete</button>';
+
+    li.onclick = function() {
+        li.classList.toggle("completed");
+    };
+     document.getElementById("taskList").appendChild(li);
+    input.value = "";
+}
+
+function deleteTask(button) {
+    button.parentElement.remove();
+}
