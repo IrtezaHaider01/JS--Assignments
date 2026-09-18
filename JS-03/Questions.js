1e62b07893686716a3459c6261cbfa6b4cf28596
IrtezaHaider01 <haiderirteza372@gmail.com>
2026-09-18 21:15:28 +0500

    Create Questions.js

---
+++ JS-03/Questions.js
@@ -0,0 +1,81 @@
+// // Question1
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+// -------------------------------------------------------------------------
+
+// // Question2
+// for (let i = 10; i >= 1; i--) {
+//     console.log(i);
+// }
+// ------------------------------------------------------------
+
+// // Question3
+// for (let i = 1; i <= 5; i++) {
+//     console.log("Hello, JavaScript!");
+// }
+// -----------------------------------------------------------------------------------------
+
+// // Question4
+// var fruits = ["Apple", "Banana", "Mango", "Orange"];
+
+// for (let i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i]);
+// }
+// ---------------------------------------------------------------------------------
+
+// // question5
+// var marks = [50, 60, 70, 80];
+// var total = 0;
+// for (let i = 0; i < marks.length; i++) {
+//     total += marks[i];
+// }
+
+// console.log("Total Marks:", total);
+// -------------------------------------------------------------------------------
+
+// // Question6
+// var marks = [50, 60, 70, 80];
+// var total = 0;
+// var average = 0;
+// for (let i= 0; i < marks.length; i++) {
+//     total += marks[i];
+// }
+// average = total / marks.length;
+
+// console.log("Average Marks:", average);
+// ------------------------------------------------------------------------------
+
+// // Question7;
+// var colors = ["Red", "Green", "Blue", "Yellow"];
+// for (let i= colors.length - 1; i >= 0; i--) {
+//     console.log(colors[i]);
+// }
+// ---------------------------------------------------------------------
+
+// // Question8
+// var numbers = [5, 10, 15, 20];
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log("Index:", i, "Element:", numbers[i]);
+// }
+// ------------------------------------------------------------------------
+
+// // Question9
+// var cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
+
+// var length = 0;
+// for (let i= 0; i < cities.length; i++) {
+//     length++;
+// }
+
+// console.log("Array Length:", length);
+// ----------------------------------------------------------
+
+// // Question10
+// var numbers = [2, 5, 8, 11, 14, 17, 20];
+
+// for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] % 2 == 0) {
+//         console.log(numbers[i]);
+//     }
+// }
